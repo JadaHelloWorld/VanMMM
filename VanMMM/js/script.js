60f4b7d83094d8152d845f2cc7f309e735c4c600
Jada <jadajin3@gmail.com>
2016-05-30 18:02:19 -0700

    Front-end side simulates register, login & logout

--- VanMMM/js/script.js
+++ VanMMM/js/script.js
@@ -1,6 +1,7 @@
 var vanMMM = angular.module('vanMMM', ['ngRoute']);
  
-var myEl = angular.element( document.querySelector( '#home' ) );;
+var myEl = angular.element( document.querySelector( '#home' ) );
+var username = "admin";
 // create the controller and inject Angular's $scope
 vanMMM.config(['$routeProvider', function($routeProvider){
     $routeProvider
@@ -20,16 +21,26 @@ vanMMM.config(['$routeProvider', function($routeProvider){
             controller : 'LoginCtrl',
             templateUrl : 'views/login.html'
         })
+        .when('/logout', {
+            controller : 'LogoutCtrl',
+            templateUrl : 'views/home.html'
+        })
         .when('/register', {
             controller : 'RegisterCtrl',
             templateUrl : 'views/register.html'
         })
-        .otherwise({redirectTo: '/'});
+        .otherwise({redirectTo: '/home'});
     
 }]);  
 
-vanMMM.controller('MainCtrl', ['$scope', function($scope){
+vanMMM.controller('MainCtrl', ['$scope', '$location', function($scope, $location){
+	setCookie("name", "admin", 1);
 	
+	$scope.isUsername = function(){
+		username = getCookie('name');
+		$scope.username = username;
+		return username !== "admin"? true: false;
+	}
     $scope.date = new Date();
 }]);
 
@@ -56,48 +67,91 @@ vanMMM.controller('ContactUsCtrl', ['$scope', function($scope){
     myEl.addClass('active');
 }]);
 
-vanMMM.controller('LoginCtrl', ['$scope', function($scope){
+vanMMM.controller('LoginCtrl', ['$scope', '$location', function($scope, $location){
     $scope.title = "login";
     myEl.removeClass('active');
     myEl = angular.element( document.querySelector( '#login' ) );
     myEl.addClass('active');
+    $scope.submit = function() {
+  		  username = $scope.login.username;
+  		  setCookie("name", username, 1);
+  		  $location.path('/home');
+    }
 }]);
 
-vanMMM.controller('RegisterCtrl', ['$scope', function($scope){
+vanMMM.controller('LogoutCtrl', ['$scope', '$location', function($scope, $location){
+    $scope.title = "logout";
+    myEl.removeClass('active');
+    myEl = angular.element( document.querySelector( '#logout' ) );
+    setCookie("name", "admin", 1);
+    username = "admin";
+    $location.path('/home');
+    
+    
+}]);
+
+vanMMM.controller('RegisterCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
     $scope.onViewLoad = function(){
       console.log('view changed');  
     };
     myEl.removeClass('active');
     myEl = angular.element( document.querySelector( '#register' ) );
     myEl.addClass('active');
+    $scope.submit = function() {
+  	  $scope.name = $scope.signup.username;
+		  $scope.email = $scope.signup.email;
+
+		  username = $scope.signup.username;
+		  setCookie("name", username, 1);
+		  $location.path('/home');
+//  	  $http({
+//	        method  : 'post',
+//	        url     : '/signup',
+//	        data    : $httpParamSerializerJQLike({
+//              name: $scope.name,
+//              email:  $scope.email
+//          }),
+//	        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+//	      })
+//	        .success(function(data) {
+//	            console.log(data);
+//	 
+//	            if (!data.success) {
+//	                // if not successful, bind errors to error variables
+//	                $scope.errorName = data.errors.name;
+//	                $scope.errorSuperhero = data.errors.superheroAlias;
+//	            } else {
+//	                // if successful, bind success message to message
+//	                $scope.message = data.message;
+//	            }
+//	      });
+  	};
 }]);
 
-vanMMM.controller('register', ['$scope', '$http', 'httpParamSerializerJQLike', function($scope, $http, httpParamSerializerJQLike) {
-      $scope.submit = function() {
-    	  $scope.name = $scope.signup.username;
-		  $scope.email = $scope.signup.email;
-    	  $http({
-	        method  : 'post',
-	        url     : '/signup',
-	        data    : $httpParamSerializerJQLike({
-                name: $scope.name,
-                email:  $scope.email
-            }),
-	        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-	      })
-	        .success(function(data) {
-	            console.log(data);
-	 
-	            if (!data.success) {
-	                // if not successful, bind errors to error variables
-	                $scope.errorName = data.errors.name;
-	                $scope.errorSuperhero = data.errors.superheroAlias;
-	            } else {
-	                // if successful, bind success message to message
-	                $scope.message = data.message;
-	            }
-	      });
-    	};
-	  
-	  
-	}]);
+//设置cookie
+function setCookie(name,value,iDay){	//三个参数分别为：cookie名称、值、多少天后过期
+	var oDate=new Date();
+
+	//这里的设置时间并不是修改系统时间，而是oDate这个日期对象内部的时间。使用这个方法可以算出该日期的几天后的日期时间
+	oDate.setDate(oDate.getDate()+iDay);
+	document.cookie=name+'='+value+';expires='+oDate;
+};
+
+
+//读取cookie
+function getCookie(name){
+	var arr=document.cookie.split('; ');
+	for(var i=0;i<arr.length;i++){
+		var arr2=arr[i].split('=');
+
+		//拆分后，arr2[0]就代表cookie的name名称，arr[1]就代表cookie的value值
+		if(arr2[0]==name){
+			return arr2[1];
+		}
+	}
+	return '';		//当用户是第一次进入网站，根本就还没有cookie的时候，返回一个空的字符串
+	
+};
+
+
+
